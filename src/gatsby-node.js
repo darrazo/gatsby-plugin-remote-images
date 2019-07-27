d0e3b6b548565fc5374c56852b42833024684d2a
darrazo <darrazo16@gmail.com>
2019-07-27 01:15:59 +0100

    Coding style fixes and missed parentNodeId addition when creating remote file

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -14,76 +14,78 @@ exports.onCreateNode = async (
     ext = null,
   } = options
 
-  let fileNodes;
-  let urls;
+  let fileNodes
+  let urls
   if (node.internal.type === nodeType) {
     try {
-      const arrayPath = imagePath.split(".");
-      urls = deepfirstsearch(node, arrayPath);
+      const arrayPath = imagePath.split(".")
+      urls = deepfirstsearch(node, arrayPath)
       fileNodes = await Promise.all(urls.map(urlObject => {
         const param = {
           url: ext ? `${urlObject.url}${ext}` : urlObject.url,
+          parentNodeId: urlObject.parentNode.id,
           store,
           cache,
           createNode,
           createNodeId,
           auth,
-          ext
+          ext,
         }
-        const promise = createRemoteFileNode(param);
-        promise.then(result => urlObject.fileNode = result);
-        return promise;
-      }));
+        const promise = createRemoteFileNode(param)
+        promise.then(result => urlObject.fileNode = result)
+        return promise
+      }))
     } catch (e) {
-      console.error('gatsby-plugin-remote-images ERROR:', e);
+      console.error('gatsby-plugin-remote-images ERROR:', e)
     }
   } // Adds a field `localImage` or custom name to the node
   // ___NODE appendix tells Gatsby that this field will link to another node
   if (fileNodes){
     urls.map(urlObject => {
-        urlObject['parentNode'][`${name}___NODE`] = urlObject.fileNode.id;
+        urlObject['parentNode'][`${name}___NODE`] = urlObject.fileNode.id
       }
     )
   }
 }
 
 const deepfirstsearch = (node, arrayPath) => {
-  const stack = [{'node': node, 'arrayIndex': 0}];
-  let currentNode;
-  let base;
-  let urls = [];
+  const stack = [{'node': node, 'arrayIndex': 0}]
+  let currentNode
+  let base
+  let urls = []
   while (stack.length > 0){
-    currentNode = stack.pop();
+    currentNode = stack.pop()
     base = arrayPath[currentNode.arrayIndex]
-    const child = get(currentNode.node, base);
-    const index = currentNode.arrayIndex + 1;
+    const child = get(currentNode.node, base)
+    const index = currentNode.arrayIndex + 1
     if (child){
       if (index == arrayPath.length){
         if (Array.isArray(child)){
           child.filter(typeof value === 'string').map(value => {
            urls.push(
             {'parentNode': currentNode.node,
-             'url': value})
-            });
+             'url': value,})
+            })
         }
         else{
           if (typeof child === 'string'){
             urls.push({'parentNode': currentNode.node,
-            'url': child});
+            'url': child,
+            })
           }
         }
       }
       else{
         if (typeof child === 'object'){
           if (Array.isArray(child)){
-            child.map(childNode => stack.push({node: childNode, arrayIndex: index}));
+            child.map(childNode => stack.push({node: childNode, arrayIndex: index,}))
           }
           else{
-            stack.push({node: child, arrayIndex: index});
+            stack.push({node: child, arrayIndex: index,})
           }
         }
       }
     }
   }
-  return urls;
+  return urls
 }
